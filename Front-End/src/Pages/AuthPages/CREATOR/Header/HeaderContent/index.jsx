da7d5b0faa6dcbee369e9024c2d24bcd876effb7
Fahd-Aguenouz <faguenouz@gmail.com>
2024-06-07 21:15:38 +0100

    fixing the header for all users

--- Front-End/src/Pages/AuthPages/CREATOR/Header/HeaderContent/index.jsx
+++ Front-End/src/Pages/AuthPages/CREATOR/Header/HeaderContent/index.jsx
@@ -11,7 +11,6 @@ import Notification from './Notification';
 import MobileSection from './MobileSection';
 
 // project import
-import { GithubOutlined } from '@ant-design/icons';
 
 // ==============================|| HEADER - CONTENT ||============================== //
 
@@ -22,17 +21,7 @@ export default function HeaderContent() {
     <>
       {!downLG && <Search />}
       {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
-      <IconButton
-        component={Link}
-        href="https://github.com/codedthemes/mantis-free-react-admin-template"
-        target="_blank"
-        disableRipple
-        color="secondary"
-        title="Download Free Version"
-        sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
-      >
-        <GithubOutlined />
-      </IconButton>
+      
 
       <Notification />
       {!downLG && <Profile />}
